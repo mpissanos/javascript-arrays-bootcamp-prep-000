f7b7686cde5cd54bfd3c2da505266a394ebdb05b
Manuel Pissanos <mpissano@gmail.com>
2018-08-21 17:57:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,7 +33,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-     array = array.slice(1)
+     var a = array.slice(1)
      return array
 }
 
