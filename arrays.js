7860ed042a798565f2e0b2048eab8835d31281bc
Manuel Pissanos <mpissano@gmail.com>
2018-08-21 17:30:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element ){
  var newArr = [...element, array]
-   
+   return newArr
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
@@ -11,6 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 
 function addElementToEndOfArray(array, element) {
   var newArr = [array, ...element]
+  return newArr;
 }
 
 
