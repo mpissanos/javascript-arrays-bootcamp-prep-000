b6d6f7e20620649608a68765a28830c5f4796c56
Manuel Pissanos <mpissano@gmail.com>
2018-08-21 18:01:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-      return array.unshift()
+      return array.shift()
      
    
 }
